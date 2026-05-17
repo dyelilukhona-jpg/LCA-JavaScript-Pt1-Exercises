ef17ef96336eb7cee9a11a1f97b041b3be4e8007
Lukhona Dyeli <dyelilukhona@gmail.com>
2026-05-17 22:04:08 +0200

    Add Exercise 01 - Variables and String Manipulation

---
+++ LCA-JavaScript-Pt1-Exercises/week6_ex01_variables.js
@@ -0,0 +1,20 @@
+// TODO: Create a variable called firstName and store your first name in it
+const firstName = "Lukhona";
+
+// TODO: Create a variable called lastName and store your last name in it
+const lastName = "Dyeli";
+
+// TODO: Create a variable called age using let (you'll modify this later)
+let age = "22";
+
+// TODO: Create a fullName variable that combines firstName and lastName
+let fullName = firstName + " " + lastName;
+
+// TODO: Print a greeting to the console using these variables
+console.log(
+  "Hello, my name is " + fullName + " and I am " + age + " years old.",
+);
+
+// TODO: Try changing the age variable to a different value
+age = "23";
+console.log("In July I will be " + age + " years old.");
