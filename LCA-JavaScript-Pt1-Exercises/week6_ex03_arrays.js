b4550292d83c8489a3dd6658bc470167c7f38c27
Lukhona Dyeli <dyelilukhona@gmail.com>
2026-05-19 22:49:35 +0200

    Add Exercise 03 - Array Basics

---
+++ LCA-JavaScript-Pt1-Exercises/week6_ex03_arrays.js
@@ -0,0 +1,22 @@
+// TODO: Create an empty array called colors
+let colors = [];
+
+// TODO: Add three colors to the array using push()
+colors.push("red" , "pink" , "purple");
+console.log(colors);
+
+// TODO: Create another array called numbers with five numbers
+let numbers = [5 , 2 , 4 , 1 , 3];
+console.log(numbers);
+
+// TODO: Remove the last color from the colors array
+colors.pop();
+console.log(colors);
+
+// TODO: Add a new color to the beginning of the colors array
+colors.unshift("grey");
+console.log(colors);
+
+// TODO: Print the length of both arrays
+console.log("Colors length:", colors.length);
+console.log("Numbers length:", numbers.length);
