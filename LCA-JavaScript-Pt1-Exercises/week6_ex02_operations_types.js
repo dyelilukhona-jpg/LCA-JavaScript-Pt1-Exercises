829c50a1a1b5b83778c2e7a04fefe125f9d523f1
Lukhona Dyeli <dyelilukhona@gmail.com>
2026-05-19 20:46:36 +0200

    Add Exercise 02 - Number Operations and Type Conversion

---
+++ LCA-JavaScript-Pt1-Exercises/week6_ex02_operations_types.js
@@ -0,0 +1,21 @@
+// TODO: Create a variable called number1 and store any number in it
+const number1 = 23;
+
+// TODO: Create a variable called number2 and store a different number in it
+const number2 = 45;
+
+// TODO: Convert number1 to a string and store it in a new variable
+const number1String = number1.toString();
+
+// TODO: Add number1 and number2 and store the result in a variable called sum
+const sum = number1 + number2;
+
+// TODO: Multiply number1 and number2 and store the result in a variable called product
+const product = number1 * number2;
+
+// TODO: Print all results to the console
+console.log("Number1:" , number1);
+console.log("Number2:" , number2);
+console.log("Number 1 as string" , number1String);
+console.log("Sum: " , sum);
+console.log("Products:" , product);
