32b45eea4e2f5a8eb7904ca1df529132dd51749a
Lukhona Dyeli <dyelilukhona@gmail.com>
2026-05-20 21:42:31 +0200

    Add Exercise 04 - Conditional Logic

---
+++ LCA-JavaScript-Pt1-Exercises/week6_ex04_conditionals.js
@@ -0,0 +1,17 @@
+// TODO: Create a variable called score and give it a value between 0 and 100
+const score = 7;
+
+// TODO: Create a variable called passingGrade and set it to 60
+const passingGrade = 60;
+
+// TODO: Add another condition to check if the score is perfect (100)
+if (score === 100) {
+  console.log("Perfect Score");
+  console.log("Passed");
+} else if (score >= passingGrade) {
+  // TODO: Write an if statement to check if score is greater than or equal to passingGrade
+  console.log("Grade Passed");
+} else {
+  // TODO: Add an else statement for when the score is below passingGrade
+  console.log("Grade Failed");
+}
